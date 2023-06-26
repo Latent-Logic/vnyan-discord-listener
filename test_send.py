7a37471868fa72b3fa96c3308993fabdf9483002
Latent Logic <lat.logic@gmail.com>
2023-06-25 23:38:24 -0700

    Allow specifying commands in config to call as =cmd

--- test_send.py
+++ test_send.py
@@ -4,8 +4,7 @@ import logging
 import aiohttp
 import discord
 import toml
-from discord.ext import commands
-from discord.ext.commands import CommandError, Context
+from discord.ext.commands import Bot, CommandError, Context
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s\t%(levelname)-7s\t%(name)s\t%(message)s")
 discord_logger = logging.getLogger("discord")
@@ -21,7 +20,7 @@ intents.bans = False
 intents.voice_states = False
 intents.typing = False
 
-bot = commands.Bot(command_prefix="?", description=description, intents=intents)
+bot = Bot(command_prefix="?", description=description, intents=intents)
 
 
 @bot.event
@@ -31,7 +30,7 @@ async def on_ready():
     for guild in bot.guilds:
         guild: discord.Guild
         log.debug(f"\tGuild {guild.name}, {guild.id} with {guild.member_count} members")
-        if guild.id not in SETTINGS["guilds"]:
+        if str(guild.id) not in SETTINGS["guilds"]:
             log.info(f"\t Not listening in {guild.name} as I don't have a config for it")
             continue
         guild_settings = SETTINGS["guilds"][str(guild.id)]
@@ -65,22 +64,79 @@ async def on_command_error(ctx: Context, exception: Exception):
         log.error(f"`{ctx.author}` sent `{ctx.message.content}` which raised error `{exception}`")
 
 
-def perm_check(ctx: Context):
-    if not ctx.guild:
+def perm_check(guild: discord.Guild, channel: discord.TextChannel, author: discord.Member):
+    if not guild:
         raise CommandError(f"This command has to run in a server context")
-    if ctx.guild.id not in SETTINGS["guilds"]:
+    if str(guild.id) not in SETTINGS["guilds"]:
         raise CommandError("No config known for this server")
-    guild_settings = SETTINGS["guilds"][str(ctx.guild.id)]
-    if not set(guild_settings["roles"]) & set(r.id for r in ctx.author.roles):
+    guild_settings = SETTINGS["guilds"][str(guild.id)]
+    if not set(guild_settings["roles"]) & set(r.id for r in author.roles):
         raise CommandError("This command only for approved roles")
-    if ctx.channel.id not in guild_settings["channels"]:
+    if channel.id not in guild_settings["channels"]:
         raise CommandError("This command only works in specific channels")
+
+
+@bot.event
+async def on_message(message: discord.Message):
+    await bot.process_commands(message)
+    if message.author.bot or not message.content.startswith("="):
+        return  # Only listening to non-bot users and =commands
+    try:
+        perm_check(message.guild, message.channel, message.author)
+    except CommandError as err:
+        log.debug(f"Ignoring {message} because {err}")
+        return
+    cmd, *rest = message.content[1:].split(maxsplit=1)  # Strip the = from the start of the message
+    if cmd not in SETTINGS["commands"]:
+        await message.channel.send(f"Unknown command {cmd}, check `?ws_list`", delete_after=30)
+        return
+    value = SETTINGS["commands"][cmd]
+    if isinstance(value, str):  # Help string, nothing more to do here
+        pass
+    elif isinstance(value, dict):
+        cmd = value.get("ws", cmd)  # Allow config to change command sent to web socket
+        arg_type = value.get("arg")
+        if arg_type == "<int>":
+            if not rest:
+                await message.channel.send(f"No argument found for {cmd} (should be a number)", delete_after=30)
+                return
+            try:
+                int(rest[0])
+            except ValueError:
+                await message.channel.send(f"Argument for {cmd} must be an number", delete_after=30)
+                return
+            cmd = f"{cmd} {rest[0]}"
+        elif arg_type == "<str>":
+            if not rest:
+                await message.channel.send(f"No argument found for {cmd}", delete_after=30)
+                return
+            cmd = f"{cmd} {rest}"
+        else:
+            if arg_type is not None:
+                ValueError(f"Command {cmd} has unknown arg {arg_type}")
+    else:
+        raise ValueError(f"Unknown configuration for command {cmd}")
+    async with aiohttp.ClientSession() as session:
+        async with session.ws_connect(SETTINGS["bot"]["vnyan_socket"]) as ws:
+            await ws.send_str(cmd)
+    await message.add_reaction("✅")
+
+
+@bot.command()
+async def ws_list(ctx: Context):
+    """List all configured websocket commands"""
+    commands = SETTINGS["commands"]
+    out_str = "\n".join(
+        f"`={k}`: {v}" if isinstance(v, str) else f'`={k}`: {v.get("help", "No Help Provided")}'
+        for k, v in commands.items()
+    )
+    await ctx.send(out_str)
 
 
 @bot.command()
 async def ws_cat(ctx: Context, data: str):
     """Echo message out to the websocket"""
-    perm_check(ctx)
+    perm_check(ctx.guild, ctx.channel, ctx.author)
     async with aiohttp.ClientSession() as session:
         async with session.ws_connect(SETTINGS["bot"]["vnyan_socket"]) as ws:
             await ws.send_str(data)
@@ -92,7 +148,7 @@ async def main(settings: dict):
         async with bot:
             await bot.start(settings["bot"]["token"])
     finally:
-        log.debug("side_shutdown shutting down sql")
+        log.debug("Shutting down bot")
 
 
 if __name__ == "__main__":
